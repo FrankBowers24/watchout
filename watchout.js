2ea32d38e55a2a29ce97f3499383e9a16b6b3a35
FrankBowers24 <FrankBowers24@gmail.com>
2015-04-03 16:06:32 -0700

    make enemies move to random location

--- watchout.js
+++ watchout.js
@@ -3,33 +3,65 @@
 //
 //
 var Watchout = function() {
-
-   this.init();
-}
+  this.init();
+};
 
 Watchout.prototype.init = function() {
-  var width = 800;
-  var height = 800;
-  var imgWidth = 100;
-  var imgHeight = 100;
-  var enemyCount = 10;
-  var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-  var img = svg.selectAll("image")
-            .data(this.randomXy(enemyCount,height, width))
+  this.width = 800;
+  this.height = 800;
+  this.imgWidth = 100;
+  this.imgHeight = 100;
+  this.enemyCount = 10;
+  this.svg = d3.select('body').append('svg')
+    .attr('width', this.width)
+    .attr('height', this.height);
+  this.img = this.svg.selectAll('image')
+            .data(this.randomXy(this.enemyCount,this.height, this.width))
             .enter()
-            .append("image")
-            .attr("width", imgWidth)
-            .attr("height", imgHeight)
-            .attr("x", function(d){ return d[0];})
-            .attr("y", function(d){ return d[1];})
-            .attr("xlink:href", "asteroid.png");
-  console.log("random array --> " + this.randomXy(10, 10, 10));
+            .append('image')
+            .attr('width', this.imgWidth)
+            .attr('height', this.imgHeight)
+            .attr('x', function(d){ return d[0];})
+            .attr('y', function(d){ return d[1];})
+            .attr('xlink:href', 'asteroid.png');
 
-}
+};
+
+Watchout.prototype.update = function(data){
+  console.dir (this);
+  var enemy = d3.select(this);
+  var enemies = this.svg.selectAll('image').data(data);
+  enemies.transition().duration(1000)
+      .tween('custom', this.tweenFunc);
+  return false;
+
+};
+
+Watchout.prototype.tweenFunc = function(endPosition){
+  //console.dir (this);
+  var enemy = d3.select(this);
+  var start = {
+    x: +enemy.attr('x'),
+    y: +enemy.attr('y')
+  };
+  var end = {
+    x: endPosition[0],
+    y: endPosition[1]
+  };
+  return function(t){
+    var enemyNextPos = {
+        x: start.x + (end.x - start.x) * t,
+        y: start.y + (end.y - start.y) * t
+      };
+    return enemy.attr('x',enemyNextPos.x).attr('y',enemyNextPos.y);
+  };
+
+};
+
+
 
 Watchout.prototype.randomXy = function(enemyCount, height, width){
+  console.log("in set tiome out  ----> " + enemyCount );
   var arrayOut = [];
   for(var i = 0; i < enemyCount; i++){
     var x = Math.random()*width;
@@ -37,6 +69,21 @@ Watchout.prototype.randomXy = function(enemyCount, height, width){
     arrayOut.push([x, y]);
   }
   return arrayOut;
-}
+};
 
 var watchout = new Watchout();
+
+//call update every secon
+// this.update(this.randomXy)
+//setInterval(watchout.update(watchout.randomXy(watchout.enemyCount,watchout.height, watchout.width)), 100);
+//d3.timer( watchout.update(watchout.randomXy(watchout.enemyCount,watchout.height, watchout.width)), 100 );
+
+
+setInterval(function(){
+watchout.update(watchout.randomXy(watchout.enemyCount,watchout.height, watchout.width))
+console.log("*************************");
+}, 1000);
+
+
+
+
